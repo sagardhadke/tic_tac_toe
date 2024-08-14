bede7a1232dff21dd7d9910cc5ebbcf2b212ec00
Sagar Dhadke <peaceunique2003@gmail.com>
2024-08-14 12:07:23 +0530

    Remove Unused Code

---
+++ lib/Dashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyDashboard extends StatefulWidget {
+  const MyDashboard({super.key});
+
+  @override
+  State<MyDashboard> createState() => _MyDashboardState();
+}
+
+class _MyDashboardState extends State<MyDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
