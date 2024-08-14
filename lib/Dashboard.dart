e3fe9af99f1c7562d7045dceed64585a6f1828dc
Sagar Dhadke <peaceunique2003@gmail.com>
2024-08-14 12:27:52 +0530

    Added Container and GridView

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -8,6 +8,10 @@ class MyDashboard extends StatefulWidget {
 }
 
 class _MyDashboardState extends State<MyDashboard> {
+
+  List ofXO = List.generate(9, (index) => "");
+  bool whoTurn = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +19,32 @@ class _MyDashboardState extends State<MyDashboard> {
         title: Text("Tic Tac Toe"),
         backgroundColor: Colors.amber,
       ),
+
+
+    body: Center(
+      child: GridView(
+        shrinkWrap: true, //fix to the screen size
+        physics: NeverScrollableScrollPhysics(), // for stop the scroll
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        
+        children: [
+          for(int i=0; i<9; i++)...{
+              
+              GestureDetector(onTap: (){
+                  
+              },
+              
+              child: Container(
+                color: Colors.amber,
+              ),
+              )
+          },
+        ],
+
+        ),
+    ),
+
     );
   }
 }
